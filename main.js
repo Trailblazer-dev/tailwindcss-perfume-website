d25f018ce9db1d8bc9b7e388a8bec458ec16b5ff
Trailblazer-dev <richvictor830@gmail.com>
2024-08-17 13:19:26 +0300

    done building

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const navMenu = document.getElementById('nav-menu');
+const navLink = document.querySelectorAll('.nav-link');
+const menuBar = document.getElementById('menu-bar');
+menuBar.addEventListener('click',()=>{
+    navMenu.classList.toggle('left-[0]')
+    menuBar.classList.toggle('ri-close-large-line')
+})
 
+navLink.forEach(link => {
+    link.addEventListener("click",() => {
+        navMenu.classList.toggle('left-[0]');
+        menuBar.classList.toggle('ri-close-large-line');
+    })
+})
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
